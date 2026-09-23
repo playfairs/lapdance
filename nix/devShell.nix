439a68354e0d37e16b2fe40de2e5edc13af4e6d9
playfairs <root@playfairs.cc>
2026-09-23 01:23:36 -0500

    build: align Nox tasks with LDC outputs

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -1,14 +1,8 @@
 { pkgs, lib }:
 
 let
-  dToolchain = lib.optionals (pkgs.stdenv.hostPlatform.isLinux) [ pkgs.dmd pkgs.dub ];
+  dToolchain = [ pkgs.ldc ];
 in
 pkgs.mkShell {
   packages = [ pkgs.nox pkgs.git ] ++ dToolchain;
-
-  shellHook = ''
-    if ! command -v dmd >/dev/null 2>&1; then
-      echo "warning: D compiler not available in this shell; install dmd from the official D installer or use a Linux host." >&2
-    fi
-  '';
 }
